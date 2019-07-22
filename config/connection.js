83d96b9fb41a90fe3a318a6da0609f17fda75108
Adam Grise <adamwgrise@gmail.com>
2019-07-21 22:58:49 -0500

    Updated database config for Heroku Jaws, trying to fix crash on run

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,11 @@
 var mysql = require("mysql");
 
 var connection = mysql.createConnection({
-  host: "localhost",
+  host: "m7nj9dclezfq7ax1.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
   port: 3306,
-  user: "root",
-  password: null,
-  database: "burgerdb"
+  user: "aiur1j8rk0f4wtvi",
+  password: "em0f5gtn6kxd0kr7",
+  database: "q63aj1zpw3udvgfj"
 });
 
 connection.connect(function(err) {
